6ae9c3dd5ec3c2f3bec7560a401193c210f2ddcf
cjy <1404340013@qq.com>
2024-10-29 17:33:56 +0800

    fix:修改打包配置

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -8,7 +8,7 @@ import Lottie from "react-lottie";
 import { cn } from "@/lib/utils";
 
 import { BackgroundGradientAnimation } from "./GradientBg";
-import { GridGlobe } from "./GridGlobe";
+// import { GridGlobe } from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import { MagicButton } from "./MagicButton";
 
@@ -113,7 +113,6 @@ export const BentoGridItem = ({
           )}
         </div>
         {id === 6 && (
-          // add background animation , remove the p tag
           <BackgroundGradientAnimation>
             <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
           </BackgroundGradientAnimation>
@@ -138,7 +137,7 @@ export const BentoGridItem = ({
           </div>
 
           {/* for the github 3d globe */}
-          {id === 2 && <GridGlobe />}
+          {/* {id === 2 && <GridGlobe />} */}
 
           {/* Tech stack list div */}
           {id === 3 && (
